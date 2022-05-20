1cd277807f9c7c8b7550d6807f973ac4defd5f47
Emilio <marqetm@gmail.com>
2022-05-19 23:47:44 -0500

    commit tree 2bf9a138818cf9d43b5b34c9d6e1c519e21768f9

---
+++ index.js
@@ -0,0 +1,14 @@
+const divide = (num1, num2) => {
+  return num1/num2;
+}
+divide(2000, 100)
+
+const square = (num) => {
+  return num * 2;
+}
+square(2);
+
+const add = (num1, num2) => {
+  return num1 + num2;
+}
+add(2, 5);
